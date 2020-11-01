1c5d2d37474350a2049391d61f45675a446bf1e8
Devon Marie Layton <dml4h@c00.cs.mtsu.edu>
2020-11-01 17:43:52 -0600

    partially working with 1 proccess

--- project5/proj5.c
+++ project5/proj5.c
@@ -8,8 +8,8 @@
 #define ENDPROCS 0
 void initArrays(float **A, float **B, int numRows, int numCols, float topTemp, float leftTemp, float rightTemp, float bottomTemp);
 float iterative(float **arrA, float **arrB, int rows, int cols, float epsilon);
-void matrixPrint(float **arrA);
-
+void matrixPrint(float **arrA, int numRows, int numCols);
+float procGrid(float **arrA, float **arrB, int rows, int cols, float epsilon, int rowStart, int rowEnd);
 //int keepWorking = 1;
 int main(int argc, char **argv)
 {
@@ -51,55 +51,64 @@ int main(int argc, char **argv)
         arrB[i] = calloc(numCols, sizeof(float *));
     }
     initArrays(arrA, arrB, numRows, numCols, topTemp, leftTemp, rightTemp, bottomTemp);
-
+	//iterative(arrA, arrB, numRows, numCols, epsilon);
     /////////////Begin Process Work/////////////////////////////////////////////////
-    sprintf(buffer, "Id:%d rowWise%d leftOver%f ", myId, numRowToWork, leftOver); //convert proc info to string & copy into buffer
+    sprintf(buffer, "STARTING WORK"); //convert proc info to string & copy into buffer
 	/////////////If I am Manager, coordinate activities/////////////////////////
-   // for (i = 1; i < numRanks; i++)
-   // {
-   //     keepWorkingBuff[i] = numRanks;
-   // }
-   
 	if (myId == managerRank)
    {                          // if manager, recieve messages from other ranks
       printf("%s\n", buffer); // print manager proccess(0) info
-      //MPI_Recv(buffer,BUFLEN , MPI_CHAR, MPI_ANY_SOURCE, tag, MPI_COMM_WORLD, &status); // i represents proc source that sent the message
+      initArrays(arrA, arrB, numRows, numCols, topTemp, leftTemp, rightTemp, bottomTemp);
       for (i = 1; i < numRanks; i++)
       {                                                                        // retrieve & print results sorted by rank
          int rowStart = (i-1)*numRowToWork;	//start grid work with id +1 to get proper values
 		 int nextIdStart = i*numRowToWork;
-		 int rowEnd = nextIdStart -1;
-		 int sendToNextId = rowEnd;
-		 //if(myId == numRanks-1)
-		//	printf("here");
-			//nextIdStart= 1; //special case when only one worker
-		 printf("MANAGER myId %d, rowStart = %d nextIdStart = %d rowEndTag = %d\n", i, rowStart, nextIdStart, rowEnd); 
-		 MPI_Send(&rowStart,numRowToWork, MPI_FLOAT, i, i, MPI_COMM_WORLD);// send rowStart with current proc id
-		 MPI_Send(&arrA[numRowToWork],numRowToWork, MPI_FLOAT, i, rowEnd, MPI_COMM_WORLD);//send array to proc id= i, with row to end 
-		 MPI_Send(&arrA[rowEnd],numRowToWork, MPI_FLOAT, i, nextIdStart, MPI_COMM_WORLD); //send nextProc currId's last row to use in its calculations
-		 //tag = i;                                                              // accept tag(proc id) from other proc's
-         MPI_Recv(&buffer, BUFLEN, MPI_CHAR, i, i, MPI_COMM_WORLD, &status); // i represents proc source that sent the message
-         //printf("%s \n", buffer);
-         printf("Manager Buff%s tag %d \n", buffer, status.MPI_TAG);
+		 int rowEndTag = nextIdStart -1;
+		 int sendToNextId;//send extra row to next Id unless last rank
+		 if( i == numRanks -1)
+		 	 sendToNextId = 1;
+		 else
+			sendToNextId = i + 1;
+		 printf("MANAGER sending id %d, rowStart = %d nextIdStart = %d rowEndTag = %d sendToNextId= %d\n", i, rowStart, nextIdStart, rowEndTag, sendToNextId); 
+		 MPI_Send(&numRowToWork,1, MPI_FLOAT, i, i, MPI_COMM_WORLD);                                    // *send which row to start; send rowStart with current proc id
+		 MPI_Send(&rowStart,1, MPI_FLOAT, i, i, MPI_COMM_WORLD);									// *send which row to start; send rowStart with current proc id
+		 //MPI_Send(&arrA[numRowToWork],numRowToWork, MPI_FLOAT, i, rowEndTag, MPI_COMM_WORLD);		// *send array & tag to end send array to proc id= i, with row to end 
+	     //MPI_Send(&arrB[0],numRowToWork, MPI_FLOAT, i, rowEndTag, MPI_COMM_WORLD); 
+      }
+	  for(i = 1; i < numRanks; i++){
+	  	MPI_Recv(&buffer, BUFLEN, MPI_CHAR, i, i, MPI_COMM_WORLD, &status); // i represents proc source that sent the message
+		printf("%s Manager Recieved from tag %d \n\n", buffer, status.MPI_TAG);
       }
    }
    ///////////If I am Worker, perform Calculations///////////////////////////
    //Send workers #of rows & neighbors?
+   float result = 0;
    if (myId != managerRank)
    {                                                               // need this condition, otherwise processors hang
       int rowStart = (myId-1)*numRowToWork;
 	  int nextIdStart = myId*numRowToWork;
-	  int rowEnd = nextIdStart -1;
-	  int sendToNextId = rowEnd;
-	  MPI_Recv(&rowStart, numRowToWork, MPI_FLOAT, 0,myId , MPI_COMM_WORLD, &status);
-	  MPI_Recv(&arrA[numRowToWork], numRowToWork, MPI_FLOAT, 0,rowEnd, MPI_COMM_WORLD, &status);
-	  MPI_Recv(&arrA[rowEnd], numRowToWork, MPI_FLOAT, 0,nextIdStart, MPI_COMM_WORLD, &status);
-	 // matrixPrint(arrA);
-	  printf("WORKER myId %d, rowStart = %d nextIdStart = %d rowEndTag = %d\n", myId, rowStart, nextIdStart, rowEnd);
-	  printf("Worker %d tag %d \n", myId, status.MPI_TAG);
+	  int rowEndTag = nextIdStart -1;
+	  int recvFrom;
+	  if(myId == 1)
+		recvFrom = numRanks -1; 
+	  else
+		recvFrom = myId -1;
+      MPI_Recv(&numRowToWork, 1, MPI_FLOAT, 0, myId , MPI_COMM_WORLD, &status);
+	  MPI_Recv(&rowStart, 1, MPI_FLOAT, 0, myId , MPI_COMM_WORLD, &status); //recieve start row & tag indicating I should be working on this chunk
+	  //MPI_Recv(&arrA[numRowToWork], numRowToWork, MPI_FLOAT, 0,rowEndTag, MPI_COMM_WORLD, &status);	// recv array & tag indicating which row to end working
+	  //MPI_Recv(&arrB[0], numRowToWork, MPI_FLOAT, 0,rowEndTag, MPI_COMM_WORLD, &status);
+	  //matrixPrint(arrB, numRows, numCols);
+	  
+	  printf("\n\nWORKER myId %d, rowStart = %d nextIdStart = %d rowEndTag = %d\n ", myId, rowStart, nextIdStart, rowEndTag);
 	  tag = myId;                                                  // identify curr message as curr proc's id
-      
+	  printf("numRow %d, numCols %d, toWork %d, epsilon %f \n", numRows, numCols, numRowToWork, epsilon);
+	  if(numRanks == 2)
+		result = procGrid(arrA,arrB,numRowToWork, numCols, epsilon,rowStart,rowEndTag);
+      else
+		result = iterative(arrA, arrB, numRowToWork, numCols, epsilon);
+	  sprintf(buffer, "Manager Recv message from Worker Id:%d result:%f rowStart%d numRowstoWork= %d nextIdStart %d extraRowrecvFrom Id %d", myId,result, rowStart, numRowToWork, nextIdStart, recvFrom); //convert proc info to string & copy into buffer
 	  MPI_Send(&buffer, BUFLEN, MPI_CHAR, 0, tag, MPI_COMM_WORLD); // send curr buff to manager
+
    } 
 
 	printf("*********out of loop Id %d\n", myId);
@@ -168,12 +177,75 @@ void initArrays(float **A, float **B, int numRows, int numCols, float topTemp, f
             A[r][c] = interiorGridVal;
     }
 }
-
-float iterative(float **arrA, float **arrB, int rows, int cols, float epsilon)
-{
+float procGrid(float **arrA, float **arrB, int rows, int cols, float epsilon, int rowStart, int rowEnd)
+{   //printf("here func rows = %d cols = %d eps= %f\n", rows, cols, epsilon);
     int endLoop = 0; //true when value less than epsilon
     float diff;
-    for (int indx = 0; !endLoop; indx++)
+	//float **arrB;
+	//arrB = arrA;
+    printf("rows %d, cols %d sizeOfA %ld sizeofB %ld\n", rows, cols, sizeof(arrA), sizeof(arrB));
+    //matrixPrint(inB, rows, cols);
+	for (int indx = 0; !endLoop; indx++)
+    {   
+        int rowIndex, colIndex;
+
+        float lgBase = 2.0;
+        float lgRes;
+        float maxDiff = 0.0;
+        float absDiff = 0.0; // absolute value of newGridVal - currGridIndexVal
+        float neighborSum;   //sum of current gridIndex's north, east, south, west neighbor values
+        float newGridVal;
+        //float **arrB = arrA;
+		float **temp = NULL;
+        for (rowIndex = 1; rowIndex < rows-1; rowIndex++)
+        {   
+            for (colIndex = 1; colIndex < cols - 1; colIndex++)
+            {   
+            //   printf("here in 3rd loop func\n"); 
+               neighborSum = arrA[rowIndex - 1][colIndex] + arrA[rowIndex + 1][colIndex] +
+                              arrA[rowIndex][colIndex - 1] + arrA[rowIndex][colIndex + 1]; 
+                newGridVal = neighborSum / 4.0;
+                absDiff = fabsf(newGridVal - arrA[rowIndex][colIndex]);
+                if (absDiff > maxDiff)
+                {   
+                    maxDiff = absDiff;
+                    // printf("Abs:%f   Max:%f   New:%f  Grid: %f  Neighbor:%f\n", absDiff, maxDiff, newGridVal, arrA[rowIndex][colIndex], neighborSum);
+                }   
+              //  printf("here in possible error\n");
+                arrB[rowIndex][colIndex] = newGridVal;
+             //   printf("here in possible error\n");
+            }   
+        }   
+
+        lgRes = modff(log2f(indx), &lgBase);
+        if (lgRes == 0 && indx != 0) //print value if indx is log base 2
+        {   
+            printf("%6d %f\n", indx, maxDiff);
+            //return maxDiff;
+        }   
+
+        temp = arrB;
+        arrB = arrA;
+        arrA = temp;
+		
+        if (maxDiff < epsilon)
+        {   
+            printf("%6d %f \n", indx, maxDiff);
+            endLoop = 1;
+            diff = maxDiff;
+			//return diff;
+           // break;
+        }   
+        //printf("here func end\n");
+    }   
+    return diff; 
+}
+float iterative(float **arrA, float **arrB, int rows, int cols, float epsilon)
+{   //printf("here func rows = %d cols = %d eps= %f\n", rows, cols, epsilon);
+    int endLoop = 0; //true when value less than epsilon
+    float diff;
+    printf("rows %d, cols %d\n", rows, cols);
+	for (int indx = 0; !endLoop; indx++)
     {
         int rowIndex, colIndex;
 
@@ -188,7 +260,8 @@ float iterative(float **arrA, float **arrB, int rows, int cols, float epsilon)
         {
             for (colIndex = 1; colIndex < cols - 1; colIndex++)
             {
-                neighborSum = arrA[rowIndex - 1][colIndex] + arrA[rowIndex + 1][colIndex] +
+    		  // printf("here in 3rd loop func\n"); 
+	           neighborSum = arrA[rowIndex - 1][colIndex] + arrA[rowIndex + 1][colIndex] +
                               arrA[rowIndex][colIndex - 1] + arrA[rowIndex][colIndex + 1];
                 newGridVal = neighborSum / 4.0;
                 absDiff = fabsf(newGridVal - arrA[rowIndex][colIndex]);
@@ -197,15 +270,17 @@ float iterative(float **arrA, float **arrB, int rows, int cols, float epsilon)
                     maxDiff = absDiff;
                     // printf("Abs:%f   Max:%f   New:%f  Grid: %f  Neighbor:%f\n", absDiff, maxDiff, newGridVal, arrA[rowIndex][colIndex], neighborSum);
                 }
-                arrB[rowIndex][colIndex] = newGridVal;
+                //printf("here in possible error\n");
+				arrB[rowIndex][colIndex] = newGridVal;
+				//printf("here in possible error\n");
             }
         }
 
         lgRes = modff(log2f(indx), &lgBase);
         if (lgRes == 0 && indx != 0) //print value if indx is log base 2
         {
-            //printf("%6d %f\n", indx, maxDiff);
-            return maxDiff;
+            printf("%6d %f\n", indx, maxDiff);
+            //return maxDiff;
         }
 
         temp = arrB;
@@ -214,24 +289,26 @@ float iterative(float **arrA, float **arrB, int rows, int cols, float epsilon)
 
         if (maxDiff < epsilon)
         {
-            //printf("%6d %f \n", indx, maxDiff);
+            printf("%6d %f \n", indx, maxDiff);
             endLoop = 1;
             diff = maxDiff;
-            return diff;
-            break;
+           // return diff;
+           // break;
         }
+	    // printf("here func end\n");
     }
     return -1;
 }
-void matrixPrint(float **arrA)
+
+void matrixPrint(float **arrB,int numRows,int numCols)
 {
     printf("\n_________________STARTING___________________________________________________\n");
-    int numRows = 10, numCols = 10;
-    for (int i = 0; i < 10; i++)
+    printf("print numRows %d, numCols %d\n", numRows, numCols);
+    for (int i = 0; i < numRows; i++)
     {
-        for (int j = 0; j < 10; j++)
+        for (int j = 0; j <numCols; j++)
         {
-            printf(" %.6f ", arrA[i][j]);
+            printf(" %.6f ", arrB[i][j]);
         }
         printf("\n");
     }
